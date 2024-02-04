778bf04819de288df2cedbfd81e72bd509541252
zichen-fei <feizichen@live.com>
2024-02-04 11:46:31 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -68,3 +68,11 @@ var _hmt = _hmt || [];
 	var s = document.getElementsByTagName("script")[0]; 
 	s.parentNode.insertBefore(hm, s);
 })();
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WNWP46LV');</script>
+<!-- End Google Tag Manager -->
